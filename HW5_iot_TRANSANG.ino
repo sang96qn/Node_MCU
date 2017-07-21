43c097ac0f7d670109424a01948e32de3c744b4a
Tran Sang <transang96qn@gmail.com>
2017-07-21 11:26:29 +0700

    Blynk app
    Send data from hardware to app
    and app to hardware

---
+++ HW5_iot_TRANSANG.ino
@@ -0,0 +1,63 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+const char *ssid = "viettel1234";
+const char* pass = "1223334444";
+char auth[] ="97adedf7831747ef8c971f8c6e5a3e6b";
+
+int buttonPushCounter =0;
+int buttonState =0;
+int lastButtonState=0;
+
+bool check =false;
+BLYNK_WRITE(V5)
+{
+  int pinValue = param.asInt(); 
+  Serial.print("V5 value is: ");
+  Serial.println(pinValue);
+  if(pinValue == 1)
+  check =true;
+  else check =false;
+   Serial.println(check);
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+ pinMode(0,INPUT);
+ pinMode(16,OUTPUT);
+ digitalWrite(16,1); 
+ Blynk.begin(auth,ssid,pass);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Blynk.run();
+  buttonState = digitalRead(0);
+if(buttonState != lastButtonState )
+{
+ // Serial.println("lastButton != buttonState");
+  if(buttonState == HIGH)
+  {
+    buttonPushCounter++;
+  //  Serial.println("press");
+    }
+    else {
+      Serial.println("Release");
+      if(buttonPushCounter %2 ==0)
+      {
+        Serial.println("Tat led");
+        Blynk.virtualWrite(V5,0);
+        check = false;
+        }
+        else {
+          Serial.println("Bat led");
+        Blynk.virtualWrite(V5,1);
+         check =true;
+          }
+      }
+  }
+  lastButtonState = buttonState;
+  if(check == 1)
+  digitalWrite(16,0);
+  else digitalWrite(16,1);
+
+}
