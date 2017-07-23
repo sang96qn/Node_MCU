3ada7228dae17177eeb864ff650e9fea6439b9ae
Tran Sang <transang96qn@gmail.com>
2017-07-23 17:44:28 +0700

    blynk app
    update

--- HW5_iot_TRANSANG.ino
+++ HW5_iot_TRANSANG.ino
@@ -7,18 +7,26 @@ char auth[] ="97adedf7831747ef8c971f8c6e5a3e6b";
 int buttonPushCounter =0;
 int buttonState =0;
 int lastButtonState=0;
-
+bool isFirstConnect = true;
 bool check =false;
 BLYNK_WRITE(V5)
 {
   int pinValue = param.asInt(); 
-  Serial.print("V5 value is: ");
-  Serial.println(pinValue);
   if(pinValue == 1)
   check =true;
   else check =false;
-   Serial.println(check);
+  // Serial.println(check);
+   buttonPushCounter++;
 }
+
+BLYNK_CONNECTED() {
+if (isFirstConnect) {
+  Blynk.syncAll();
+  Blynk.notify("TIMER STARTING!!!!");
+isFirstConnect = false;
+}
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -34,14 +42,12 @@ void loop() {
   buttonState = digitalRead(0);
 if(buttonState != lastButtonState )
 {
- // Serial.println("lastButton != buttonState");
   if(buttonState == HIGH)
   {
     buttonPushCounter++;
-  //  Serial.println("press");
     }
     else {
-      Serial.println("Release");
+    //  Serial.println("Release");
       if(buttonPushCounter %2 ==0)
       {
         Serial.println("Tat led");
